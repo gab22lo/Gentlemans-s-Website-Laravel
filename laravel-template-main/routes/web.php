e230004bc4aa9f74c9a431217c898eb79dafd2c9
Kauaiduha <111883950+Kauaiduha@users.noreply.github.com>
2022-10-13 17:38:58 -0300

    Changing routes
    Adicionando rotas na pagina principal

--- laravel-template-main/routes/web.php
+++ laravel-template-main/routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
+
+Route::get('/professores', function () {
+    return view('professor');
+})->name("professor");
+

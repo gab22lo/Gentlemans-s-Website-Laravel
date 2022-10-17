5f0621008d4374c4b6d9fc7e87c4d3874d58506b
Kauaiduha <111883950+Kauaiduha@users.noreply.github.com>
2022-10-17 19:21:22 -0300

    Adding file for "Create" function
    Created a file who is called in AlunoControllerCreate and AlunoControllerStore

---
+++ laravel-template-main/resources/views/alunos/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrando aluno</title>
+</head>
+<body>
+    <from action="{{ route('registrar_aluno') }}" method="POST">
+        @csrf
+        <label for="">Nome</label> <br />
+        <input type="text" name="nome"> <br />
+        <label for="">RA</label> <br />
+        <input type="text" name="RA"> <br />
+        <label for="">Materia</label> <br />
+        <input type="text" name="Materias"> <br />
+        <label for="">Filme</label> <br />
+        <input type="text" name="FF"> <br />
+        <button>Salvar</button>
+    </form>
+</body>
+</html>

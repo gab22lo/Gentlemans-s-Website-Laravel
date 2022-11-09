9a0726bdc0dbd8d910ef1d82a44bd5b87fe780ac
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-09 15:40:37 -0300

    Update create.blade.php
    updatre create.blade

--- laravel-template-main/resources/views/alunos/create.blade.php
+++ laravel-template-main/resources/views/alunos/create.blade.php
@@ -1,23 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrando aluno</title>
+    <title>Cadastrar um novo Aluno</title>
 </head>
 <body>
-    <from action="{{ route('registrar_alunos') }}" method="POST">
-        @csrf
-        <label for="">Nome</label> <br />
-        <input type="text" name="nome"> <br />
-        <label for="">RA</label> <br />
-        <input type="text" name="RA"> <br />
-        <label for="">Materia</label> <br />
-        <input type="text" name="Materias"> <br />
-        <label for="">Filme</label> <br />
-        <input type="text" name="FF"> <br />
-        <button>Salvar</button>
+    <form action="{{ route('registrar_aluno') }}" method="POST">
+        @csrf 
+        <label for="">CPF</label> <br />
+        <input type="text" name="CPF"> <br />
+        <label for="">Nome Completo</label> <br />
+        <input type="text" name="Nome Completo"> <br />
+        <label for="">CEP</label> <br />
+        <input type="text" name="CEP"> <br />
+        <label for="">Rua</label> <br />
+        <input type="text" name="Rua"> <br />
+        <label for="">Bairro</label> <br />
+        <input type="text" name="Bairro"> <br />
+        <label for="">Cidade</label> <br />
+        <input type="text" name="Cidade"> <br />
+        <label for="">Número</label> <br />
+        <input type="text" name="Número"> <br />
+        <label for="">Filme Favorito</label> <br />
+        <input type="text" name="filme favorito"> <br />
+        <button> Salvar</button>
     </form>
 </body>
 </html>

fe02091924845e697f9d4eb29a5e456bdebbccb1
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-16 16:13:09 -0300

    Update web.php

--- laravel-template-main/routes/web.php
+++ laravel-template-main/routes/web.php
@@ -13,3 +13,7 @@ Route::get('/professores', function() {
 
 Route::get('/alunos', 'App\Http\Controllers\AlunosController@create');
 Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("registrar_aluno");
+
+
+Route::get('/materias', 'App\Http\Controllers\MateriasController@create');
+Route::post('/materias', 'App\Http\Controllers\MateriasController@store')->name("registrar_materia");

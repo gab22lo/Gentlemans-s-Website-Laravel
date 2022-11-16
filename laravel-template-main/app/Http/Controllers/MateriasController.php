a0093d3c17bcaeaba1191016c54aaaf77f3e9883
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-16 16:03:55 -0300

    Create MateriasController.php

---
+++ laravel-template-main/app/Http/Controllers/MateriasController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\materia;
+
+class MateriasController extends Controller
+{
+    public function create()
+    {
+        return view('materias.create');
+    }
+
+    public function store(Request $request)
+    {
+        materia::create([
+            'Nome' => $request->Nome,
+            'Desc_c' => $request->Desc_c,
+            'Desc_s' => $request->Desc_s,
+            'Num_min' => $request->Num_min,
+            'Num_max' => $request->Num_max,
+            'Status' => $request->Status,
+            'Professor' => $request->Professor,
+        ]);
+
+    return "Matéria Criada com Sucesso!";
+
+    }
+}

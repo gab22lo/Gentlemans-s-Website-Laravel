19d7a1f7c0de3683c269bc1ccff52cc7a76aeec9
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-22 20:59:51 -0300

    Update web.php

--- laravel-template-main/routes/web.php
+++ laravel-template-main/routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 }) -> name("home");
 
-Route::get('/professores', function() {
-    return view('professores');
-}) -> name("professores");
+Route::get('/principal', function() {
+    return view('principal.create');
+}) -> name("principal");
 
 Route::get('/alunos', 'App\Http\Controllers\AlunosController@create');
 Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("registrar_aluno");
@@ -17,3 +17,10 @@ Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("reg
 
 Route::get('/materias', 'App\Http\Controllers\MateriasController@create');
 Route::post('/materias', 'App\Http\Controllers\MateriasController@store')->name("registrar_materia");
+
+Route::get('/professores', 'App\Http\Controllers\ProfessoresController@create');
+Route::post('/professores', 'App\Http\Controllers\ProfessoresController@store')->name("registrar_professores");
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('lista_alunos','lista_alunos');

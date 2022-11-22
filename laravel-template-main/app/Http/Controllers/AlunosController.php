911aab602e239d282e2c59c1ac8873467c17b567
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-22 20:48:18 -0300

    Update AlunosController.php
    add user login

--- laravel-template-main/app/Http/Controllers/AlunosController.php
+++ laravel-template-main/app/Http/Controllers/AlunosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\aluno;
+use App\Models\User;
 
 class AlunosController extends Controller
 {
@@ -14,18 +15,28 @@ class AlunosController extends Controller
 
     public function store(Request $request)
     {
-    aluno::create([
-        'CPF' => $request->CPF,
-        'Nome' => $request->Nome,
-        'CEP' => $request->CEP,
-        'Rua' => $request->Rua,
-        'Bairro' => $request->Bairro,
-        'Cidade' => $request->Cidade,
-        'Número' => $request->Número,
-        'Filme'=> $request->Filme,
-     ]);
+        aluno::create([
+            'CPF' => $request->CPF,
+            'Nome' => $request->Nome,
+            'CEP' => $request->CEP,
+            'Rua' => $request->Rua,
+            'Bairro' => $request->Bairro,
+            'Cidade' => $request->Cidade,
+            'Número' => $request->Número,
+            'Filme'=> $request->Filme,
+        ]);                     
+      
+    
+        User::create([
+        
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> $request->password,
+        ]);
+        //$user = User::first();
+        //$user->assignRole('aluno');
 
     return "Aluno Criado com Sucesso!";
-
+    
     }
 }

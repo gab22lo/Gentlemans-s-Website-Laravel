16f574f48f8097af9bc051b4d8baf443a571f1f6
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-22 21:16:02 -0300

    Add files via upload

---
+++ laravel-template-main/database/migrations/2022_11_22_140452_create_professors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professores', function (Blueprint $table) {
+            $table->id();
+            $table->string('CPF')->unique();
+            $table->string('Nome');
+            $table->string('CEP');
+            $table->string('Rua');
+            $table->string('Bairro');
+            $table->string('Cidade');
+            $table->integer('Número');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professores');
+    }
+};

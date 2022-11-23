d9c188a461a38f3643c15a36a43b96ead8381945
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-23 18:54:08 -0300

    Update web.php

--- laravel-template-main/routes/web.php
+++ laravel-template-main/routes/web.php
@@ -14,13 +14,17 @@ Route::get('/principal', function() {
 Route::get('/alunos', 'App\Http\Controllers\AlunosController@create');
 Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("registrar_aluno");
 
+Route::get('/User','App\Http\Controllers\Auth\RegisterController@validator')->name("Registrar_user");
+Route::post('/User','App\Http\Controllers\Auth\RegisterController@make');
 
 Route::get('/materias', 'App\Http\Controllers\MateriasController@create');
 Route::post('/materias', 'App\Http\Controllers\MateriasController@store')->name("registrar_materia");
 
 Route::get('/professores', 'App\Http\Controllers\ProfessoresController@create');
 Route::post('/professores', 'App\Http\Controllers\ProfessoresController@store')->name("registrar_professores");
-//Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('lista_alunos','lista_alunos');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

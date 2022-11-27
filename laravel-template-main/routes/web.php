2fac3c4d862a4dcf43526606fdefa270d34a9a69
Pedrao <111129585+pedrao1605@users.noreply.github.com>
2022-11-27 01:03:54 -0300

    Update web.php

--- laravel-template-main/routes/web.php
+++ laravel-template-main/routes/web.php
@@ -7,31 +7,61 @@ Route::get('/', function () {
     return view('welcome');
 }) -> name("home");
 
-Route::get('/perfil', function() {
-    return view('principal.create');
-}) -> name("aluno");
+
 
 Route::get('/alunos', 'App\Http\Controllers\AlunosController@create');
 Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("registrar_aluno");
 
-Route::get('/User','App\Http\Controllers\Auth\RegisterController@validator')->name("Registrar_user");
-Route::post('/User','App\Http\Controllers\Auth\RegisterController@make');
+Route::get('/alunos', 'App\Http\Controllers\AlunosController@create');
+Route::post('/alunos', 'App\Http\Controllers\AlunosController@store')->name("registrar_aluno");    
 
-Route::get('/materias', 'App\Http\Controllers\MateriasController@create');
+
+
+
+Route::get('/principal', function() {
+    return view('principal.create');
+}) -> name("principal");
+
+
+
+
+Route::middleware(['secretaria'])->group(function(){
+    route::get('professores', function(){
+
+        return view('professores.create');
+
+    });
+});
+
+Route::middleware(['secretaria'])->group(function(){
+    route::get('alunos', function(){
+
+        return view('alunos.create');
+
+    });
+});
+
+Route::middleware(['secretaria'])->group(function(){
+    route::get('materias', function(){
+
+        return view('materias.create');
+
+    });
+});
+
+
 Route::post('/materias', 'App\Http\Controllers\MateriasController@store')->name("registrar_materia");
 
-Route::get('/professores', 'App\Http\Controllers\ProfessoresController@create');
-Route::post('/professores', 'App\Http\Controllers\ProfessoresController@store')->name("registrar_professores");
 
+Route::post('/professores', 'App\Http\Controllers\ProfessoresController@store')->name("registrar_professores");
+//Auth::routes();
+
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('lista_alunos','lista_alunos');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', function() {
-    return view('principal.home');
-}) -> name("Home");
-
-Route::get('/User','App\Http\Controllers\Auth\RegisterController@validator')->name("Registrar_user");
-Route::post('/User','App\Http\Controllers\Auth\RegisterController@make');

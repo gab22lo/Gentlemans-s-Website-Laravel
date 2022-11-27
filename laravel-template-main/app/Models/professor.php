4e19c2c2fb8d593314db5675c74e0f5fa82966ce
Pedrao <111129585+pedrao1605@users.noreply.github.com>
2022-11-27 00:57:48 -0300

    Update professor.php

--- laravel-template-main/app/Models/professor.php
+++ laravel-template-main/app/Models/professor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class professor extends Model
+class professore extends Model
 {
     protected $fillable = ['CPF', 'Nome', 'CEP', 'Rua', 'Bairro', 'Cidade', 'Número'];
-} 
+}

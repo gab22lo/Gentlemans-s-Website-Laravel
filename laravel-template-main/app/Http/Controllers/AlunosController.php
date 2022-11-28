94075cc2b4cf18817ea293574c6bd7c525e355e6
gab22lo <111184993+gab22lo@users.noreply.github.com>
2022-11-27 21:02:29 -0300

    Update AlunosController.php

--- laravel-template-main/app/Http/Controllers/AlunosController.php
+++ laravel-template-main/app/Http/Controllers/AlunosController.php
@@ -8,6 +8,13 @@ use App\Models\User;
 
 class AlunosController extends Controller
 {
+
+    public function index()
+    {
+        $alunos = Aluno::all();
+        return view ('alunos.index')->with('alunos', $alunos);
+    }
+
     public function create()
     {
         return view('alunos.create');
@@ -29,4 +36,48 @@ class AlunosController extends Controller
         return view('auth.register');
     
     }
+
+    public function show($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.show', ['aluno' => $aluno]);
+    }
+
+    public function edit($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.edit', ['aluno' => $aluno]);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $aluno = Aluno::findOrFail($id);
+
+        $aluno->update([
+                'CPF' => $request->CPF,
+                'Nome' => $request->Nome,
+                'CEP' => $request->CEP,
+                'Rua' => $request->Rua,
+                'Bairro' => $request->Bairro,
+                'Cidade' => $request->Cidade,
+                'Número' => $request->Número,
+                'Filme'=> $request->Filme,
+        ]);
+        return redirect('/indexalunos');
+    }
+
+   public function delete($id)
+   {
+    $aluno = Aluno::findOrFail($id);
+    return view('alunos.delete', ['aluno' => $aluno]);
+   }
+
+   public function destroy($id)
+   {
+    $aluno = Aluno::findOrFail($id);
+    $aluno->delete();
+
+    return redirect('/indexalunos');
+   }
 }
